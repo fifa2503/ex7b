e65eb2035684ceaca742f6a62b1433e030b89855
Achitapon <achitaponfifa@gmail.com>
2025-03-18 22:39:24 +0700

    Bug fix#2 – adding student id validation (id must be composed of numbers between 0 and 9

--- validate.js
+++ validate.js
@@ -1,11 +1,14 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
+	let sid = (document.getElementById("sid").value).trim();
+	
+	const regex = /^[0-9]{10}$/;
+	
+	if (regex.test(sid)) {
+	  return true;
+	} else {
+	  return false;
+	}
   }
-}
 
 function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
